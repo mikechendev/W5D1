7a7d94c009b903b66dca92f5ddb204345b8e91ba
Michael Chen <michael.chen.1120@gmail.com>
2022-04-25 16:23:03 -0400

    Add hashmap lru cache

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,106 @@
+class MaxIntSet
+  attr_reader :store
+  def initialize(max)
+    @store = Array.new(max)
+  end
+
+  def insert(num)
+    raise 'Out of bounds' if num > @store.length || num < 0
+    return @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+attr_reader :store
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    return false if include?(num)
+    @store[num % num_buckets] << num
+    return true
+  end
+
+  def remove(num)
+    @store[num].delete(num)
+  end
+
+  def include?(num)
+    @store[num % num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    return false if include?(num)
+    @store[num % num_buckets] << num
+    @count += 1
+    resize! if @count > @store.length
+  end
+
+  def remove(num)
+    return if !include?(num)
+    @store[num % num_buckets].delete(num)
+    @count -= 1
+  end
+
+  def include?(num)
+    @store[num % num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) {Array.new}
+    old_store.flatten.each do |num|
+      insert(num)
+    end
+  end
+end
